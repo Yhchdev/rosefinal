23981dfed11d8380ec5ae230e3c72594865d6a39
Yhchdev <630673943@qq.com>
2019-08-16 11:49:08 +0800

    发送控制指令完善

--- src/main/java/com/yhchdev/rosesorting/controller/controlController.java
+++ src/main/java/com/yhchdev/rosesorting/controller/controlController.java
@@ -1,14 +1,7 @@
 package com.yhchdev.rosesorting.controller;
-
-
-
-
-
-
+import com.alibaba.fastjson.JSONObject;
 import com.yhchdev.rosesorting.gateway.MqttGateway;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,9 +10,26 @@ public class controlController {
     @Autowired
     private MqttGateway mqttGateway;
 
-    @RequestMapping(value ="/control/1")
-    public void assess()
+    // 重拍图片
+    @RequestMapping(value ="/control/re")
+    public void re()
     {
-        mqttGateway.send("1","hello");
+        JSONObject jsonObj = new JSONObject();
+        jsonObj.put("state",0);
+        jsonObj.put("model",2);
+        jsonObj.put("msg","re do detection");
+        String data = jsonObj.toString();
+        mqttGateway.send(data,"control");
+    }
+
+    //评估模式
+    @RequestMapping(value = "/control/assess")
+    public void assess(){
+        JSONObject jsonObj = new JSONObject();
+        jsonObj.put("state",0);
+        jsonObj.put("model",3);
+        jsonObj.put("msg","评估");
+        String data = jsonObj.toString();
+        mqttGateway.send(data,"control");
     }
 }
